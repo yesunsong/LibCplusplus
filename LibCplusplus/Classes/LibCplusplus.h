2361e3aa2b6eec08c25441172e33e2d291845ca0
Sunsong Ye <1042282500@qq.com>
2016-11-24 18:17:55 +0800

    提交 mac下的c++库

---
+++ LibCplusplus/Classes/LibCplusplus.h
@@ -0,0 +1,13 @@
+//
+//  LibCplusplus.h
+//  LibCplusplus
+//
+//  Created by Sunsong Ye on 16/11/24.
+//  Copyright © 2016年 Sunsong Ye. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LibCplusplus : NSObject
+
+@end
---
+++ LibCplusplus/Classes/LibCplusplus.m
@@ -0,0 +1,13 @@
+//
+//  LibCplusplus.m
+//  LibCplusplus
+//
+//  Created by Sunsong Ye on 16/11/24.
+//  Copyright © 2016年 Sunsong Ye. All rights reserved.
+//
+
+#import "LibCplusplus.h"
+
+@implementation LibCplusplus
+
+@end
